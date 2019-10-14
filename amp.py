6807cae965d4d0e024afce587fb702eefbedddf0
JS-0125 <56055661+JS-0125@users.noreply.github.com>
2019-10-15 00:16:47 +0900

    commit : 3
    캐릭터 공격 모션 추가

--- amp.py
+++ amp.py
@@ -1,33 +1,59 @@
 from pico2d import*
 
+def handle_events():
+    global running
+    global dir
+    global frameX
+    global frameY
+    events = get_events()
+
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key== SDLK_a:
+                frameY = 0
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                 dir -= 1
+            elif event.key == SDLK_LEFT:
+                 dir += 1
+
+
 open_canvas(512,1024)
 
 grass = load_image('20180417022947-1-576x1024.jpg')
 character = load_image('Mobile - Cookie Run - Roll Cake Cookie.png')
 
 x = 0
-frame = 0
+dir = 0
+frameX = 0
+frameY = 4
+running = True
 
-while (x < 800):
-    clear_canvas()
-    grass.draw(238, 512)
+while running:
+    while (x < 512):
+        clear_canvas()
+        grass.draw(238, 512)
+        character.clip_draw(frameX * 360, frameY * 360, 250, 250, x, 500)
+        update_canvas()
+        handle_events()
 
-    events = get_events()
+        if(frameY == 4):
+            frameX = (frameX + 1) % 8
+        elif (frameY == 0):
+            frameX = (frameX + 1) % 19
+            if( frameX == 0):
+                frameY = 4
 
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_RIGHT:
-            x += 5
-            frame = (frame + 1) % 8
-            running = False
-
-    character.clip_draw(frame * 360, 1450, 250, 250, x, 500)
-    update_canvas()
-
-
-    delay(0.05)
-    get_events()
+        x += dir * 5
+        delay(0.03)
 
 
-close_canvas()
+    close_canvas()
