6d43e03afd065ddc0d198c2d923dce223dd29a9a
JS-0125 <56055661+JS-0125@users.noreply.github.com>
2019-11-17 01:33:06 +0900

    commit : 57
    물건 팔고 리스트에서 삭제

--- inventory.py
+++ inventory.py
@@ -1,6 +1,7 @@
 from pico2d import *
 import crystal
 
+
 class Inventory:
     def __init__(self):
         self.image = load_image('resourse/inventory.png')
@@ -30,21 +31,27 @@ class Inventory:
         if 40 < x < 200 and 235 < y < 260:
             if len(self.my_bag) >= 1:
                 self.sell(0)
+                self.my_bag.remove(0)
         elif 200 < x < 360 and 235 < y < 260:
             if len(self.my_bag) >= 2:
                 self.sell(1)
+                self.my_bag.remove(1)
         elif 360 < x < 515 and 235 < y < 260:
             if len(self.my_bag) >= 3:
                 self.sell(2)
+                self.my_bag.remove(2)
         elif 40 < x < 200 and 35 < y < 60:
             if len(self.my_bag) >= 4:
                 self.sell(3)
+                self.my_bag.remove(3)
         elif 200 < x < 360 and 35 < y < 60:
             if len(self.my_bag) >= 5:
                 self.sell(4)
+                self.my_bag.remove(4)
         elif 360 < x < 515 and 35 < y < 60:
             if len(self.my_bag) >= 6:
                 self.sell(5)
+                self.my_bag.remove(5)
 
     def sell(self, i):
         if self.my_bag[i] == crystal:
